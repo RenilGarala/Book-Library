7c3e781918dce2c5e07774ec318b6f22bcd133d9
renil <renilgarala360004@gmail.com>
2025-03-25 01:47:32 +0530

    Toggle between list view and grid view

--- app.js
+++ app.js
@@ -39,21 +39,25 @@ async function setBooks(boookData) {
       <div class="thumbnail">
         <img src="${imagePath}">
       </div>
-      <div class="book-author">
-        By ${bookAuthor}
-      </div>
-      <div class="book-title">
-        ${bookTitle}
-      </div>
-      <div class="publisher">
-        Saler : ${bookPublisher}
-      </div>
-      <div class="published-date">
-        Date : ${publishDate}
+      <div class="book-info">
+        <div class="book-author">
+          By ${bookAuthor}
+        </div>
+        <div class="book-title">
+          ${bookTitle}
+        </div>
+        <div class="publisher">
+          <b>Saler :</b> ${bookPublisher}
+        </div>
+        <div class="published-date">
+          <b>Date :</b> ${publishDate}
+        </div>
       </div>
     `;
     bookList.appendChild(bookCard);
   });
+
+  setView();
 }
 
 //call search funtion on every chnage inpute value
@@ -62,6 +66,7 @@ search.addEventListener("input", () => {
   searchBook();
 });
 
+//search funtion it's campare with title and author both
 function searchBook() {
   const input = document.querySelector(".searchBook").value.toLowerCase();
   const bookList = document.querySelectorAll(".book");
@@ -77,3 +82,38 @@ function searchBook() {
     }
   });
 }
+
+function setView() {
+  //Toggle btn for grid view and list view
+  const toggleBtn = document.getElementById("toggleBtn");
+  const bookContainer = document.querySelector(".book-list");
+  const bookList = document.querySelectorAll(".book");
+  let isGridView = false;
+
+  toggleBtn.addEventListener("click", () => {
+    isGridView = !isGridView;
+
+    if (isGridView) {
+      toggleBtn.innerText = "Grid View";
+      bookList.forEach((book) => {
+        book.style.flexDirection = "initial"; 
+        bookContainer.style.display = "flex";
+        bookContainer.style.flexDirection = "column"; 
+        book.style.width = "700px";
+        bookContainer.style.alignItems = "center";
+      });
+    }
+    else{
+      toggleBtn.innerText = "List View";
+      bookList.forEach((book) => {
+        book.style.flexDirection = "column"; 
+        bookContainer.style.display = "grid";
+        bookContainer.style.flexDirection = "row";
+        book.style.width = "100%"; 
+        bookContainer.style.alignItems = "normal"; 
+      });
+    }
+  });
+}
+
+//toggle between list view and grid is crazy.
